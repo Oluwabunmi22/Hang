5d8525fbdc4dd2190a8778619137cb0428b0e36f
Oluwabunmi22 <88315868+Oluwabunmi22@users.noreply.github.com>
2021-08-04 11:51:08 +0100

    Add files via upload

---
+++ program.java
@@ -0,0 +1,10 @@
+public class Program 
+{
+    public static void main(String[] args) 
+    {
+        System.out.println( "Oluwabunmi Adeniyi");
+        System.out.println ("oluwabunmipra@gmail.com");
+        System.out.println ("@Oluwabunmi");
+        System.out.println ("Public Health and Genomic Epidemiology");
+    }
+}
